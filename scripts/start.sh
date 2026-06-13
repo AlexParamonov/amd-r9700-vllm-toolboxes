4e8282a0466c564a65de95fea62a7327f5d04a09
Alexander Paramonov <352030+AlexParamonov@users.noreply.github.com>
2026-06-13 03:44:26 +0200

    Final version

--- scripts/start.sh
+++ scripts/start.sh
@@ -48,6 +48,11 @@ export VLLM_ENABLE_INDUCTOR_MAX_AUTOTUNE=1
 export SAFETENSORS_FAST_GPU=1
 export VLLM_DISABLE_COMPILE_CACHE=1
 # export VLLM_LOGGING_LEVEL=DEBUG
+export TOKENIZERS_PARALLELISM=true
+export OMP_NUM_THREADS=12
+export MKL_NUM_THREADS=12
+export OPENBLAS_NUM_THREADS=12
+
 
 # Detect R9700 GPUs and set HIP_VISIBLE_DEVICES (matches start_vllm.py behavior)
 gfx1201_indices=()
@@ -71,7 +76,7 @@ vllm serve Qwen/Qwen3.6-27B-FP8 --host 0.0.0.0 --port 8079 --tensor-parallel-siz
   --dtype auto --trust-remote-code --language-model-only \
   --gpu-memory-utilization 0.95 --max-num-batched-tokens 16384 \
   --enable-auto-tool-choice --tool-call-parser qwen3_coder --reasoning-parser qwen3 \
-  --speculative-config '{"method": "mtp", "num_speculative_tokens": 2}' \
+  --speculative-config '{"method": "mtp", "num_speculative_tokens": 3}' \
   --override-generation-config '{"temperature": 0.6, "top_p": 0.95, "top_k": 20}' --max-num-seqs 1 \
   --max-model-len 196608 --served-model-name qwen27 --enable-prefix-caching \
   --attention-backend TRITON_ATTN --mm-encoder-attn-backend TRITON_ATTN \
